f16ddc4b46ba651c310a6fc6f94c5114da0814d0
D404G0 <diegol-patinof@unilibre.edu.co>
2024-08-14 22:26:27 -0500

    first commit

---
+++ datos.py
@@ -0,0 +1,50 @@
+def readint( message):
+    while True:
+        try:
+            return int(input(message))
+        except ValueError:
+            print("El dato que ingreso no es numerico, intentelo nuevamente.")
+
+def readnote(mensaje):
+    try:
+        numes = float(input(mensaje))
+        if numes < 0 or numes > 5:
+            raise ValueError
+        return numes
+    except ValueError:
+        print("El valor no esta en el rando de 0 a 5.")
+
+def receivedata(numes, students):
+    for i in range(1, numes + 1):
+        name = input(f"Ingrese el nombre del estudiante numero {i}: ")
+        note2 = readnote(f"Ingrese la nota del estudiante {name}: ")
+        if note2 >= 3:
+            aprobado["Nombre"] = name
+            aprobado["Aprobo con"] = note2
+        elif note2 < 3:
+            reprobado["Nombre"] = name
+            reprobado["Reprobo con"] = note2
+
+        idest = readint(f"Ingrese la identificacion del estudiante {name}: ")
+        while idest in students:
+            print(f"{idest} ya existe, intentelo nuevamente.")
+            idest = readint(f"Ingrese la identificacion del estudiante numero {name}: ")
+        students.append(idest)
+
+        
+
+def showstudents(students):
+    print(f"Los estudiantes son: {students}")
+    print(aprobado, reprobado)
+
+if __name__ == "__main__":
+    aprobado = {}
+    reprobado = {}
+    students = []
+    while True:
+        numes = readint("Cuantos estudiantes quiere validar?: ")
+        if numes > 0:
+            break
+        print("El numero no puede ser menor a 0, intentelo nuievamente.")
+    receivedata(numes, students)
+    showstudents(students)
